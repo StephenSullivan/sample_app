f61af2f97e72ea50025f6275107f0ab48fc509df
Stephen Sullivan <steve.t.sullivan@gmail.com>
2012-10-17 14:37:41 -0400

    Users index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,9 +27,12 @@ describe "Authentication" do
   		before { sign_in(user) }
 
   		it { should have_title(user.name) }
+
+      it { should have_link('Users', href: users_path) }
   		it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
+
   		it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -42,6 +45,24 @@ describe "Authentication" do
   describe "authorization" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in')}
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+      end
 
       describe "when attempting to visit a protected page" do
         before do
@@ -56,21 +77,6 @@ describe "Authentication" do
             page.should have_selector('title', text: 'Edit user')
           end
         end
-      end
-    end
-
-    let(:user) { FactoryGirl.create(:user) }
-
-    describe "in the Users controller" do
-
-      describe "visiting the edit page" do
-        before { visit edit_user_path(user) }
-        it { should have_selector('title', text: 'Sign in')}
-      end
-
-      describe "submitting to the update action" do
-        before { put user_path(user) }
-        specify { response.should redirect_to(signin_path) }
       end
     end
 
